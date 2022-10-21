7e88608346e8cb6714863aba7266229a5279f28a
SitiNurhalizaYus <114122090+SitiNurhalizaYus@users.noreply.github.com>
2022-10-22 04:34:05 +0700

    Create latihan3.java

---
+++ src/SITI211022/latihan3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SITI211022;
+
+/**
+ *
+ * @author siti nurhaliza yus
+ */
+public class latihan3 {  //For
+    public static void main(String args[]){
+    int r;
+    for (r=0; r<100; r++){
+        System.out.println("SITI"); 
+    }
+  }
+}
