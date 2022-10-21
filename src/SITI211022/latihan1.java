b6556321031cf487c88e95720625ac58facb0f7d
SitiNurhalizaYus <114122090+SitiNurhalizaYus@users.noreply.github.com>
2022-10-22 04:34:01 +0700

    Create latihan1.java

---
+++ src/SITI211022/latihan1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SITI211022;
+
+/**
+ *
+ * @author siti nurhaliza yus
+ */
+public class latihan1 { //While
+    public static void main(String args[]){
+    int r=0;
+    while (r<100){
+        System.out.println("SITI");
+        r++;
+    }
+  }
+}
