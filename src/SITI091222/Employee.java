190325f1016d12a1d338458bd324c5e2e92bf9a2
SitiNurhalizaYus <114122090+SitiNurhalizaYus@users.noreply.github.com>
2022-12-09 11:53:21 +0700

    Create Employee.java

---
+++ src/SITI091222/Employee.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SITI091222;
+
+/**
+ *
+ * @author siti nurhaliza yus
+ */
+public class Employee extends Person  { 
+  public String getName(){ 
+    System.out.println("Employee Name : " + name); 
+    return name;     
+  }
+}
