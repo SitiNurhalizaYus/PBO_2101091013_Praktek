ae40706f031c1ff30d0782bc745d205bd5cb1533
SitiNurhalizaYus <114122090+SitiNurhalizaYus@users.noreply.github.com>
2022-12-16 12:57:42 +0700

    Create Relation.java

---
+++ src/SITI161222/Relation.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SITI161222;
+
+/**
+ *
+ * @author siti nurhaliza yus
+ */
+public interface Relation {
+    public boolean isGreater( Object a, Object b);
+    public boolean isLess( Object a, Object b);
+    public boolean isEqual( Object a, Object b); 
+}
